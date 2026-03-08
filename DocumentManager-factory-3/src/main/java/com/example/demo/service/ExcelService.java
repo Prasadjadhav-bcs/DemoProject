a3575bd3e56d5a4de5fb37d3bd34d424be112d84
Prasad Jadhav <prasadjadhav779900@gmail.com>
2026-03-08 14:54:22 +0530

    update code

--- DocumentManager-factory-3/src/main/java/com/example/demo/service/ExcelService.java
+++ DocumentManager-factory-3/src/main/java/com/example/demo/service/ExcelService.java
@@ -5,7 +5,7 @@ public class ExcelService extends DocumentSerAbs {
 	@Override
 	public void processData() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Process the data");
 	}
 
 	@Override
